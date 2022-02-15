45f46239f3c33a2530c74ef029dd2fa1ce8b46cc
Merlin <MerlinShieh@outlook.com>
2022-02-15 21:57:23 +0800

    1、增加了文件上传，修改了日志接口处理 2、并且在日志接口新增了日志(确实顶级) 3、修改了test接口的content type格式判断 4、文件展示（访问）接口没弄好 暂时挂起

--- api_JS_Py.py
+++ api_JS_Py.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import json_tools
 import json
 import time
@@ -13,9 +14,12 @@ logger = Log().logger
 executor = ThreadPoolExecutor(5)
 # 初始化线程池 5
 app = Flask(__name__)
-
-
 # 初始化flask
+
+# 配置文件上传
+__UPLOAD_FOLDER = r'./files/uploads'
+__ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+__MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
 
 
 class RespResult:
@@ -76,7 +80,7 @@ async def translate_img():
 		return RespResult.error()
 
 
-def _log(level: str, msg: any):
+def _log(msg: any, level='debug', ):
 	lel = level.lower()
 	logger.info(msg) if \
 		lel == 'info' or 'log' else logger.warning(msg) if \
@@ -87,9 +91,24 @@ def _log(level: str, msg: any):
 
 @app.route('/log', methods=['POST'])
 async def log():
+	"""
+	支持三个Content Type
+	:return:
+	"""
 	if request.method == 'POST':
-		level = str(request.form['level'])
-		message = str(request.form['message'])
+		if RespHeaders.ContentType.is_form_data():
+			level = str(request.form['level'])
+			message = str(request.form['message'])
+		elif RespHeaders.ContentType.is_json():
+			level = str(dict(request.get_json())['level'])
+			message = str(dict(request.get_json())['message'])
+		elif RespHeaders.ContentType.is_form_urlencoded():
+			level = str(dict(request.values)['level'])
+			message = str(dict(request.values)['message'])
+		else:
+			level = 'error'
+			message = '不支持的格式'
+		_log('远程日志为：{}, {}'.format(level, message))
 		executor.submit(_log, level, message)
 		return RespResult.success(data={"logger_name": logger.name})
 	else:
@@ -116,13 +135,17 @@ def _check_environment(env, glo, req):
 		f.seek(2)
 		f.write(env + '\n')
 	msg = {"env": json_tools.diff(last_env, env), "glo": json_tools.diff(last_glo, glo)}
-	_log('log', req + "：\n环境变量变化为：" + str(json.dumps(msg).encode('utf-8').decode("unicode_escape")) + "\n")
+	_log(req + "：\n环境变量变化为：" + str(json.dumps(msg).encode('utf-8').decode("unicode_escape")) + "\n")
 	return msg
 
 
 @app.route('/check_environment', methods=['POST'])
 def check_environment():
-	if request.method == 'POST':
+	"""
+	只支持 form格式
+	:return:
+	"""
+	if request.method == 'POST' and RespHeaders.ContentType.is_form_data():
 		env = str(request.form['environment'])
 		glo = str(request.form['globals'])
 		req = str(request.form['request'])
@@ -135,24 +158,70 @@ def check_environment():
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
+	"""
+	支持json form form-urlencoded 三种
+	:return:
+	"""
 	__methods = request.method
 	if True if __methods == 'POST' or 'GET' else False:
 		# noinspection PyBroadException
 		try:
 			# 判断请求的格式，json form form-urlencoded
-
 			# print(RespHeaders.ContentType.is_json())
 			# print(RespHeaders.ContentType.is_form_data())
 			# print(RespHeaders.ContentType.is_form_urlencoded())
 
-			data = (request.get_json() if RespHeaders.ContentType.is_json() else dict(request.form) if RespHeaders.ContentType.is_form_data() else dict(request.values))
-			print(data)
+			data = (request.get_json() if
+					RespHeaders.ContentType.is_json() else dict(request.form) if
+			RespHeaders.ContentType.is_form_data() else dict(request.values))
+			_log(data)
 			return RespResult.success(data=data)
 		except:
 			return RespResult.error(data=traceback.extract_stack())
 	else:
 		return RespResult.error()
 
+
+def allowed_file(filename):
+	_log('检查文件上传的格式，文件为：{}，检查格式为：{}'.format(filename, __ALLOWED_EXTENSIONS))
+	return '.' in filename and filename.rsplit('.', 1)[1] in __ALLOWED_EXTENSIONS
+
+
+@app.route('/uploadfiles', methods=['POST', 'PUT'])
+def uploadfiles():
+	"""
+	设置上传限制
+	:return:
+	"""
+	__methods = request.method
+	_log('检查到文件上传，方式为：'+__methods)
+	if True if __methods == 'POST' or 'PUT' else False:
+		# 获取文件类型的参数
+		try:
+			file = request.files['file']
+			if file and allowed_file(file.filename):
+				# filename = secure_filename(file.filename)
+				file.save(os.path.join(__UPLOAD_FOLDER, file.filename))
+				_log('文件上传成功！路径为：'+os.path.join(__UPLOAD_FOLDER, file.filename))
+			# 获取上传文件的二进制流
+			# print(f"request.files.get('file_name').read(): {request.files.get('file_name').read()}")
+			return RespResult.success()
+		except:
+			return RespResult.error(data=traceback.extract_stack())
+
+
+"""
+文件展示没有弄好
+
+@app.route('/show/<filename>', methods=['POST', 'GET'])
+def show_file(filename):
+	if True if request.method == 'POST' or 'GET' else False:
+		filename = request.values.get(filename)
+		with open(__UPLOAD_FOLDER+r'/'+filename, 'wb') as f:
+			file_base64 = base64.b64encode(f.read())
+			print(file_base64)
+			return RespResult.success(data=file_base64)
+"""
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8801, debug=True, use_reloader=True)
